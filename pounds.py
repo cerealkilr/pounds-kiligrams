48b681c0b264a1f537b120a10a8cae414c8aa392
cerealkilr <105010964+cerealkilr@users.noreply.github.com>
2022-06-07 18:36:17 -0700

    Update pounds.py

--- pounds.py
+++ pounds.py
@@ -1,2 +1,7 @@
-def kilo int(input("Pounds: ")):
-    return kilo / float(2.20462262)
+kilo = int(input("Pounds: ")):
+    kilo_2 = kilo / float(2.20462262)
+    print(f"Kilograms: {kilo_2}")
+    
+Pound = int(input("Kilograms: ")):
+    pound_2 = Pound * float(2.20462262)
+    print(f"Pounds: {Pound_2})
