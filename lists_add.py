dbbc3e175991fb45486660226599899cf2f77f5c
cerealkilr <105010964+cerealkilr@users.noreply.github.com>
2022-06-20 14:59:52 -0700

    Update and rename pounds.py to lists_add.py

---
+++ lists_add.py
@@ -0,0 +1,9 @@
+list1 = [1, 32, 43, 546]
+list2 = [3, 437, 86, 90]
+print(sum(list1 + list2))
+
+
+list1 = [1, 32, 43, 546]
+list2 = [3, 437, 86, 90]
+bothlist = list1 + list2
+print(sum(bothlist))
--- pounds.py
+++
@@ -1,7 +0,0 @@
-kilo = int(input("Pounds: ")):
-    kilo_2 = kilo / float(2.20462262)
-    print(f"Kilograms: {kilo_2}")
-    
-Pound = int(input("Kilograms: ")):
-    pound_2 = Pound * float(2.20462262)
-    print(f"Pounds: {Pound_2})
